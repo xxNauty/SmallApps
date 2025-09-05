f1519e8208502258e0c5ab829c3423371f736e44
Mateusz Wnuk <mateusz2003w@gmail.com>
2025-09-05 15:35:18 +0200

    workflow fix #6

--- tests/run_all.py
+++ tests/run_all.py
@@ -6,7 +6,6 @@ directory = "./tests"
 files_ran = 0
 
 for root, dirs, files in os.walk(directory):
-    print(root, dirs, files)
     for filename in files:
         if filename.endswith('.py') and filename != 'run_all.py':
             filepath = os.path.join(root, filename)
